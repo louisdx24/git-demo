4a86c750749f9a9695236d854e1c3f97868c26cb
louis <louisdx24@icloud.com>
2023-12-30 08:22:16 +0800

    完成主要遊戲邏輯

--- text.py
+++ text.py
@@ -1,10 +1,19 @@
 import random
 
 #1-50
+#使用者可以猜五次
+#猜對提前離開
+#猜錯則提示答案
 
-
-for i in range(10):
-    x=random.randint(1,50)
-    print(x)
+#題目
+x=random.randint(1,50)
+print(x)
+for i in range(5):
+    y=int(input('請猜數字(1-50):'))
+    if y==x:
+        print('猜對了')
+        break
+    else:
+        print('猜錯了')
 
 
