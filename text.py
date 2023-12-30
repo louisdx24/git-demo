03e78b29adc92b5d6cb7bb9be171a77370360c05
louis <louisdx24@icloud.com>
2023-12-30 19:38:41 +0800

    完成猜數字區間提示功能

--- text.py
+++ text.py
@@ -6,15 +6,29 @@ import random
 # 猜錯則提示答案
 
 # 題目
-x = random.randint(1, 50)
-print(x)
+
+start = 1
+end = 50
+
+x = random.randint(start, end)
+print(f"範圍:{start}~{end}")
+
 for i in range(5):
-    y = int(input("請猜數字(1-50):"))
+    y = int(input(f"請猜數字({start}-{end}):"))
     if y == x:
         print("猜對了")
         break
     else:
         if y > x:
             print("猜低一點")
+            if end > y:
+                end = y - 1
         else:
+            if start < y:
+                start = y + 1
             print("猜高一點")
+
+if y != x:
+    print(f"猜錯了，答案為{x}")
+else:
+    print(f"恭喜過關，共猜了{i+1}次")
